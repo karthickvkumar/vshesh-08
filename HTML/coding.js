f77036d742062d3832e0f42dbf09a524e6206efd
karthickvkumar <karthickvelraj@live.in>
2023-10-17 10:26:01 +0530

    commit message

---
+++ HTML/coding.js
@@ -0,0 +1,25 @@
+// Primitive Type
+// Number - Any Numberical Value from Positive, Negative, Decimal, Fraction
+
+// string - Any Text Value but it should be enclosed inside single or double quote
+
+// boolen - true or false 
+
+// Variable Creation (Temp storage)
+// syntax 
+// var variable_name = value;
+// var -> Keyword 
+// variable_name -> Placeholder (It should be a proper and meaningful name, contain only A-Z, a-z, 0-9, _, $)
+// value -> input data
+
+var rollnumber = 45786565; 
+
+var email_id = "kathick212@gmail.com";
+
+var result = true;
+
+// Null - Non Exist value 
+
+// undefined - A variable has been created but its value will not be assigned
+
+var future_referene;
