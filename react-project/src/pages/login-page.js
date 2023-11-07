8c7087746a22185ee54ac962aef89cad89ed2009
karthickvkumar <karthickvelraj@live.in>
2023-11-07 09:21:59 +0530

    commit message

--- react-project/src/pages/login-page.js
+++ react-project/src/pages/login-page.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 const LoginScreen = () => {
+
+  const getValueFromInput = (event) => {
+    console.log(event.target.value, event.target.id);
+  }
+
   return(
     <div>
       <h1>Login Page</h1>
+      <div>
+        <label>Enter your Email ID :</label>
+        <input type="text" id="emailIdField" placeholder="Enter your email Id" onChange={getValueFromInput} />
+      </div>
+      <div>
+        <label>Enter your Password :</label>
+        <input type="password" id="passwordField" placeholder="Enter your password" onChange={getValueFromInput} />
+      </div>
+      <button>Login</button>
     </div>
   )
 }
