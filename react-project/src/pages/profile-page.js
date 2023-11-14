87346240da62742af9f3a228522d9e914321e1a8
karthickvkumar <karthickvelraj@live.in>
2023-11-14 09:49:44 +0530

    commit message

--- react-project/src/pages/profile-page.js
+++ react-project/src/pages/profile-page.js
@@ -1,9 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 const ProfileScreen = () => {
+
+  const listAllUsers = () => {
+    const url = "https://reqres.in/api/users?page=2";
+
+    axios.get(url)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   return(
     <div>
       <h1>Profile Page</h1>
+      <button onClick={() => listAllUsers()}>List Users</button>
     </div>
   )
 }
