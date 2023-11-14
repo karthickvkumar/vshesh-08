9a38dca7833cfd9a08588d116010b2161ebe5615
karthickvkumar <karthickvelraj@live.in>
2023-11-14 10:22:06 +0530

    commit message

--- react-project/src/pages/profile-page.js
+++ react-project/src/pages/profile-page.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
+import "../css/profile.css";
+
 const ProfileScreen = () => {
+
+  const [users, updateUsers] = useState([]);
 
   const listAllUsers = () => {
     const url = "https://reqres.in/api/users?page=2";
 
     axios.get(url)
       .then((response) => {
-        console.log(response);
+        updateUsers(response.data.data);
       })
       .catch((error) => {
         console.log(error);
@@ -19,6 +23,32 @@ const ProfileScreen = () => {
     <div>
       <h1>Profile Page</h1>
       <button onClick={() => listAllUsers()}>List Users</button>
+      <table id="customers">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Picture</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map((value, index) => {
+              return(
+                <tr key={index}>
+                  <td>{value.first_name}</td>
+                  <td>{value.last_name}</td>
+                  <td>{value.email}</td>
+                  <td>
+                    <img src={value.avatar} alt="" />
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
